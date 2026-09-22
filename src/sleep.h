GabrielGanne/tcpreplay

commit 3b0934ecbe7f5276e2e700fb04ea3fef1da40282

rewrite do_sleep to handle sub sleep times by only nanosleeping

src/sleep.h
@@ -28,6 +28,7 @@
 
 #include <sys/types.h>
 #include <sys/time.h>
+#include <time.h>
 #include <unistd.h>     
 #include <errno.h>
 #include <string.h>
@@ -54,7 +55,7 @@
 
 static inline void
 nanosleep_sleep(sendpacket_t *sp _U_, const struct timespec *nap,
-        struct timeval *now,  bool flush _U_)
+        struct timespec *now,  bool flush _U_)
 {
     nanosleep(nap, NULL);
 #ifdef HAVE_NETMAP
@@ -62,7 +63,7 @@ nanosleep_sleep(sendpacket_t *sp _U_, const struct timespec *nap,
         ioctl(sp->handle.fd, NIOCTXSYNC, NULL);   /* flush TX buffer */
 #endif /* HAVE_NETMAP */
 
-    gettimeofday(now, NULL);
+    clock_gettime(CLOCK_REALTIME, now);
 }
 
 
@@ -74,36 +75,38 @@ nanosleep_sleep(sendpacket_t *sp _U_, const struct timespec *nap,
  */
 static inline void
 gettimeofday_sleep(sendpacket_t *sp _U_, struct timespec *nap,
-        struct timeval *now, bool flush _U_)
+                   struct timespec *now, bool flush _U_)
 {
-    struct timeval sleep_until, nap_for;
+    struct timespec sleep_until, nap_for;
+    init_timestamp(&nap_for);
 #ifdef HAVE_NETMAP
-    struct timeval last;
+    struct timespec last;
     uint32_t i = 0;
 
-    TIMEVAL_SET(&last, now);
+    TIMESPEC_SET(&last, now);
 #endif /* HAVE_NETMAP */
 
-    TIMESPEC_TO_TIMEVAL(&nap_for, nap);
-    timeradd(now, &nap_for, &sleep_until);
+    timeradd_timespec(now, &nap_for, &sleep_until);
     
     while (!sp->abort) {
 #ifdef HAVE_NETMAP
-        if (flush && timercmp(now, &last, !=)) {
-            TIMEVAL_SET(&last, now);
+        if (flush && timescmp(now, &last, !=)) {
+            TIMESPEC_SET(&last, now);
             if ((++i & 0xf) == 0)
                 /* flush TX buffer every 16 usec */
                 ioctl(sp->handle.fd, NIOCTXSYNC, NULL);
         }
 #endif /* HAVE_NETMAP */
-        if (timercmp(now, &sleep_until, >=))
+        if (timescmp(now, &sleep_until, >=))
             break;
 
 #ifdef HAVE_SCHED_H
         /* yield the CPU so other apps remain responsive */
         sched_yield();
 #endif
-        gettimeofday(now, NULL);
+        struct timeval now_ms;
+        gettimeofday(&now_ms, NULL);
+        TIMEVAL_TO_TIMESPEC(&now_ms, now);
     }
 }
 
@@ -115,16 +118,18 @@ gettimeofday_sleep(sendpacket_t *sp _U_, struct timespec *nap,
  * for future reference
  */
 static inline void 
-select_sleep(sendpacket_t *sp _U_, const struct timespec *nap,
-        struct timeval *now,  bool flush _U_)
+select_sleep(sendpacket_t *sp _U_, struct timespec *nap,
+        struct timespec *now_ns,  bool flush _U_)
 {
     struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
 #ifdef HAVE_NETMAP
     if (flush)
         ioctl(sp->handle.fd, NIOCTXSYNC, NULL);   /* flush TX buffer */
 #endif /* HAVE_NETMAP */
 
-    TIMESPEC_TO_TIMEVAL(&timeout, nap);
+    TIMEVAL_TO_TIMESPEC(&timeout, nap);
 
     if (select(0, NULL, NULL, NULL, &timeout) < 0)
         warnx("select_sleep() returned early due to error: %s", strerror(errno));
@@ -133,8 +138,9 @@ select_sleep(sendpacket_t *sp _U_, const struct timespec *nap,
     if (flush)
         ioctl(sp->handle.fd, NIOCTXSYNC, NULL);   /* flush TX buffer */
 #endif
-
-    gettimeofday(now, NULL);
+    struct timeval now_ms;
+    gettimeofday(&now_ms, NULL);
+    TIMEVAL_TO_TIMESPEC(&now_ms, now_ns);
 }
 #endif /* HAVE_SELECT */
 
@@ -150,6 +156,6 @@ select_sleep(sendpacket_t *sp _U_, const struct timespec *nap,
 void ioport_sleep_init(void);
 
 void ioport_sleep(sendpacket_t *sp _U_, const struct timespec *nap,
-        struct timeval *now,  bool flush);
+        struct timespec *now,  bool flush);
 
 #endif /* __SLEEP_H__ */

src/bridge.c
@@ -238,7 +238,7 @@ do_bridge(tcpbridge_opt_t *options, tcpedit_t *tcpedit)
         do_bridge_bidirectional(options, tcpedit);
     }
 
-    if (gettimeofday(&stats.end_time, NULL) < 0)
+    if (get_time_of_day(&stats.end_time) < 0)
         errx(-1, "gettimeofday() failed: %s",  strerror(errno));
     packet_stats(&stats);
 }

src/common/timer.c
@@ -86,8 +86,18 @@ void timesdiv(struct timespec *tvs, COUNTER div)
 }
 
 void
-init_timestamp(timestamp_t *ctx)
+init_timestamp(struct timespec *timestamp)
 {
-    timerclear(ctx);
+    timesclear(timestamp);
 }
 
+int get_time_of_day(struct timespec *ts) {
+    struct timeval tv;
+    int success = gettimeofday(&tv, NULL);
+    TIMEVAL_TO_TIMESPEC(&tv, ts);
+    return success;
+}
+
+int clock_get_time(struct timespec *ts){
+    return clock_gettime(CLOCK_REALTIME, ts);
+}

src/common/timer.h
@@ -117,6 +117,35 @@ void timesdiv(struct timespec *tvs, COUNTER div);
     } while (0)
 #endif
 
+/* add tvp and uvp and store in vvp */
+#ifndef timeradd_timespec
+#define timeradd_timespec(tvp, uvp, vvp)                             \
+    do {                                                    \
+        (vvp)->tv_sec = (tvp)->tv_sec + (uvp)->tv_sec;      \
+        (vvp)->tv_nsec = (tvp)->tv_nsec + (uvp)->tv_nsec;   \
+        if ((vvp)->tv_nsec >= 1000000000) {                 \
+            (vvp)->tv_sec++;                                \
+            (vvp)->tv_nsec -= 1000000000;                   \
+        }                                                   \
+    } while (0)
+#endif
+
+
+/* add tvp and uvp and store in vvp */
+#ifndef timeradd_timeval_timespec
+#define timeradd_timeval_timespec(tvp, uvp, vvp)                             \
+    do {                                                         \
+        (vvp)->tv_sec = (tvp)->tv_sec + (uvp)->tv_sec;           \
+        (vvp)->tv_nsec = (tvp)->tv_nsec + (uvp)->tv_usec * 1000; \
+        if ((vvp)->tv_nsec >= 1000000000) {                      \
+            int seconds = (vvp)->tv_nsec % 1000000000;           \
+            (vvp)->tv_sec += seconds;                            \
+            (vvp)->tv_nsec -= 1000000000 * seconds;              \
+        }                                                        \
+    } while (0)
+#endif
+
+
 /* subtract uvp from tvp and store in vvp */
 #ifndef timersub
 #define  timersub(tvp, uvp, vvp)                             \
@@ -170,7 +199,8 @@ void timesdiv(struct timespec *tvs, COUNTER div);
 
     typedef struct timeval timestamp_t;
 
-void init_timestamp(timestamp_t *ctx);
-
+void init_timestamp(struct timespec *timestamp);
+int get_time_of_day(struct timespec *ts);
+int clock_get_time(struct timespec *ts);
 
 #endif /* _TIMER_H_ */

src/common/utils.c
@@ -196,7 +196,7 @@ int _our_safe_pcap_next_ex(pcap_t *pcap, struct pcap_pkthdr **pkthdr,
 void
 packet_stats(const tcpreplay_stats_t *stats)
 {
-    struct timeval diff;
+    struct timespec diff;
     COUNTER diff_us;
     COUNTER bytes_sec = 0;
     u_int32_t bytes_sec_10ths = 0;
@@ -206,8 +206,8 @@ packet_stats(const tcpreplay_stats_t *stats)
     COUNTER pkts_sec = 0;
     u_int32_t pkts_sec_100ths = 0;
 
-    timersub(&stats->end_time, &stats->start_time, &diff);
-    diff_us = TIMEVAL_TO_MICROSEC(&diff);
+    timessub(&stats->end_time, &stats->start_time, &diff);
+    diff_us = TIMESPEC_TO_MICROSEC(&diff);
 
     if (diff_us && stats->pkts_sent && stats->bytes_sent) {
         COUNTER bytes_sec_X10;
@@ -236,13 +236,13 @@ packet_stats(const tcpreplay_stats_t *stats)
         pkts_sec_100ths = pkts_sec_X100 % 100;
     }
 
-    if (diff_us >= 1000 * 1000)
+    if (diff_us >= 1000 * 1000) {
         printf("Actual: " COUNTER_SPEC " packets (" COUNTER_SPEC " bytes) sent in %zd.%02zd seconds\n",
-                stats->pkts_sent, stats->bytes_sent, (ssize_t)diff.tv_sec, (ssize_t)(diff.tv_usec / (10 * 1000)));
-    else
+                stats->pkts_sent, stats->bytes_sent, (ssize_t)diff.tv_sec, (ssize_t)(diff.tv_nsec / (10 * 1000000)));
+    } else{ 
         printf("Actual: " COUNTER_SPEC " packets (" COUNTER_SPEC " bytes) sent in %zd.%06zd seconds\n",
-                stats->pkts_sent, stats->bytes_sent, (ssize_t)diff.tv_sec, (ssize_t)diff.tv_usec);
-
+                stats->pkts_sent, stats->bytes_sent, (ssize_t)diff.tv_sec, (ssize_t)diff.tv_nsec / 1000);
+    }
 
     if (mb_sec >= 1)
         printf("Rated: %llu.%1u Bps, %llu.%02u Mbps, %llu.%02u pps\n",
@@ -265,7 +265,7 @@ packet_stats(const tcpreplay_stats_t *stats)
  * @param len: length of the buffer
  * @return: string containing date, or -1 on error
  */
-int format_date_time(struct timeval *when, char *buf, size_t len)
+int format_date_time(struct timespec *when, char *buf, size_t len)
 {
     struct tm *tm;
     char tmp[64];
@@ -277,7 +277,7 @@ int format_date_time(struct timeval *when, char *buf, size_t len)
         return -1;
 
     strftime(tmp, sizeof tmp, "%Y-%m-%d %H:%M:%S.%%06u", tm);
-    return snprintf(buf, len, tmp, when->tv_usec);
+    return snprintf(buf, len, tmp, when->tv_nsec / 1000);
 }
 
 /**

src/common/utils.h
@@ -31,11 +31,11 @@ typedef struct {
     COUNTER bytes_sent;
     COUNTER pkts_sent;
     COUNTER failed;
-    struct timeval start_time;
-    struct timeval time_delta;
-    struct timeval end_time;
-    struct timeval pkt_ts_delta;
-    struct timeval last_print;
+    struct timespec start_time;
+    struct timespec time_delta;
+    struct timespec end_time;
+    struct timespec pkt_ts_delta;
+    struct timespec last_print;
     COUNTER flow_non_flow_packets;
     COUNTER flows;
     COUNTER flows_unique;
@@ -47,7 +47,7 @@ typedef struct {
 
 int read_hexstring(const char *l2string, u_char *hex, const int hexlen);
 void packet_stats(const tcpreplay_stats_t *stats);
-int format_date_time(struct timeval *when, char *buf, size_t len);
+int format_date_time(struct timespec *when, char *buf, size_t len);
 uint32_t tcpr_random(uint32_t *seed);
 void restore_stdin(void);
 

src/defines.h.in
@@ -317,7 +317,6 @@ typedef u_int32_t uint32_t
 #define TIMEVAL_TO_MILLISEC(x)  (((x)->tv_sec * 1000) + ((x)->tv_usec / 1000))
 #define TIMEVAL_TO_MICROSEC(x)  (((x)->tv_sec * 1000000) + (x)->tv_usec)
 #define TIMEVAL_TO_NANOSEC(x)   ((u_int64_t)((x)->tv_sec * 1000000000) + ((u_int64_t)(x)->tv_usec * 1000))
-#define TIMSTAMP_TO_MICROSEC(x) (TIMEVAL_TO_MICROSEC(x))
 
 #define MILLISEC_TO_TIMEVAL(x, tv)          \
     do {                                    \

commit d22227b3ba0346c778bf6a031de23eca5b6ad7d2

fix --pps option regression
Correct the timespec-to-timeval conversion in select_sleep() so
rate-limited replays do not run at top speed.

Fixes: 3b0934ecbe7f5276e2e700fb04ea3fef1da40282
Fixes: https://github.com/appneta/tcpreplay/issues/1150
Signed-off-by: Gabriel Ganne <gabriel.ganne@gmail.com>

src/sleep.h
@@ -117,7 +117,7 @@ gettimeofday_sleep(sendpacket_t *sp, struct timespec *nap, struct timespec *now,
  * for future reference
  */
 static inline void
-select_sleep(sendpacket_t *sp _U_, struct timespec *nap, struct timespec *now_ns, bool flush _U_)
+select_sleep(sendpacket_t *sp _U_, const struct timespec *nap, struct timespec *now_ns, bool flush _U_)
 {
     struct timeval timeout;
     timeout.tv_sec = 0;
@@ -127,7 +127,7 @@ select_sleep(sendpacket_t *sp _U_, struct timespec *nap, struct timespec *now_ns
         ioctl(sp->handle.fd, NIOCTXSYNC, NULL); /* flush TX buffer */
 #endif                                          /* HAVE_NETMAP */
 
-    TIMEVAL_TO_TIMESPEC(&timeout, nap);
+    TIMESPEC_TO_TIMEVAL(&timeout, nap);
 
     if (select(0, NULL, NULL, NULL, &timeout) < 0)
         warnx("select_sleep() returned early due to error: %s", strerror(errno));